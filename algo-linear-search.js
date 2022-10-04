568de87c4246427ecb5c12a8c53f91fcb3bfa9e1
cassiegoldstein <86804740+cassiegoldstein@users.noreply.github.com>
2022-10-04 17:17:22 -0500

    Update algo-linear-search.js

--- algo-linear-search.js
+++ algo-linear-search.js
@@ -2,7 +2,6 @@ const arrayToSearchThrough = [];
 for (let i = 1; i <= 1000; i++) {
     arrayToSearchThrough.push(i);
 }
-arrayToSearchThrough.push(4);
 
 function linearSearch(valueToFind, arrayToSearchThrough) {
     let index;
