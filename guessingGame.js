8830379b7bd18c679750701b10edac30fc7d10a8
cassiegoldstein <86804740+cassiegoldstein@users.noreply.github.com>
2022-10-26 09:49:56 -0500

    Update guessingGame.js

--- guessingGame.js
+++ guessingGame.js
@@ -49,6 +49,7 @@ function setGameOver() {
   guessField.disabled = true;
   guessSubmit.disabled = true;
   resetButton = document.createElement('button');
+  resetButton.className = "reset"
   resetButton.textContent = 'Start new game';
   document.body.append(resetButton);
   resetButton.addEventListener('click', resetGame);
